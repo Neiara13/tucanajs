11ea108de1ee16ba6398e0e35516f5ea5093eff3
Neiara <114897245+Neiara13@users.noreply.github.com>
2024-11-04 17:15:41 +0100

    Update shuffle.js

--- shuffle.js
+++ shuffle.js
@@ -1,4 +1,3 @@
-<script>
   function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
   const j = Math.floor(Math.random() * (i + 1));
@@ -28,4 +27,3 @@
   </div>
   `;
   }
-  </script>
