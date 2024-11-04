46d84f1bb3aeed4666c83fcd6d0f3f1097803231
Neiara <114897245+Neiara13@users.noreply.github.com>
2024-11-04 23:35:26 +0100

    Update data.js

--- data.js
+++ data.js
@@ -156,7 +156,7 @@
             <div class="carousel-item ${index === 0 ? "active" : ""} text-center">
               <h3>${testimonial.name}</h3>
               <h4>${testimonial.server}</h4>
-              <p>${testimonial.text}</p>
+              <p>${testimonial.name}-${testimonial.server}: ${testimonial.text}</p>
             </div>
           `)
           .join("");
