cfa9e2d1be7b7a89d8edc79f7ffabf73f56c190b
Neiara <114897245+Neiara13@users.noreply.github.com>
2024-12-08 00:44:12 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
     document.addEventListener("DOMContentLoaded", function() {
       const testimonials1 = [
+        { name: "Cen Seven", server: "Phoenix", text: "Haven't seen a place with so much love up into it in a while - the house, the staff, the website, the rooms - seriously amazing! Keep it up" },
         { name: "Corus Po'rashun", server: "Sagittarius", text: "Never have I seen such impressive handwork with the vodka. 10/10" },
         { name: "Circe Caster", server: "Omega", text: "Thank you for the amazing chimken! Got stuck in jumping puzzle so had to log off xD <3" },
         { name: "Doro Lalayon", server: "Twintania", text: "Doro was here, good place... aggghhhh jumping puzzles" },
